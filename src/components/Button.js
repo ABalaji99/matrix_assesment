aca412564656d4c91f290452ece4ee99d36e4ae8
A Balaji <amulunarayanabalaji@gmail.com>
2024-07-21 14:38:13 +0530

    second changes
    resolved nav bar responsive design issue

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Button = ({ children, onClick, className = '' }) => (
+  <button onClick={onClick} className={`px-4 py-2 rounded ${className}`}>
+    {children}
+    { ' '} {child}   
+    
+    
+    {children}
+    { ' '} {child}   
+    
+    
+    {children}
+    { ' '} {child}   
+    
+    
+    {children}
+    { ' '} {child}   
+    
+    
+    {children}
+    { ' '} {child}   
+    
+    
+    </button>
+);
+
+export default Button;
